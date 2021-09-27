937a050a92cc25ca68bdd3930a33568a73926872
Muhammad-Abdullah-Bajwa <muhamad.abdullah.bajwa@gmail.com>
2021-09-27 11:10:45 -0400

    Show updated data when new province is clicked

--- app/src/main/java/com/example/worldcovid19tracker/MainActivity.kt
+++ app/src/main/java/com/example/worldcovid19tracker/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.worldcovid19tracker
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.example.worldcovid19tracker.databinding.ActivityMainBinding
 import com.google.gson.GsonBuilder
@@ -25,6 +27,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var provincialData: Map<String, List<CovidData>>
     private lateinit var nationalDailyData: CovidData
     private lateinit var binding: ActivityMainBinding
+    private var province = "Canada"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +35,10 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        if (intent.extras?.getString("province") != null) {
+            province = intent.extras?.getString("province")!!
+        }
 
         val outputDateFormat = SimpleDateFormat("MMM dd, yyyy", Locale.US)
 
@@ -58,31 +65,69 @@ class MainActivity : AppCompatActivity() {
                 nationalDailyData = nationalData.summary[0]
                 Log.i(TAG, "Update graph with new daily national numbers")
 
-                binding.countryName.text = nationalDailyData.province
+                if (province == "Canada") {
+                    binding.countryName.text = province
 
-                binding.ConfirmedNumbers.text = NumberFormat.getInstance().format(nationalDailyData.cumulative_cases)
-                binding.ConfirmedNumbersToday.text = NumberFormat.getInstance().format(nationalDailyData.active_cases_change)
+                    binding.ConfirmedNumbers.text =
+                        NumberFormat.getInstance().format(nationalDailyData.cumulative_cases)
+                    binding.ConfirmedNumbersToday.text =
+                        NumberFormat.getInstance().format(nationalDailyData.active_cases_change)
 
-                binding.ActiveNumbers.text = NumberFormat.getInstance().format(nationalDailyData.active_cases)
-                binding.ActiveNumbersToday.text = NumberFormat.getInstance().format(nationalDailyData.active_cases_change)
+                    binding.ActiveNumbers.text =
+                        NumberFormat.getInstance().format(nationalDailyData.active_cases)
+                    binding.ActiveNumbersToday.text =
+                        NumberFormat.getInstance().format(nationalDailyData.active_cases_change)
 
-                binding.RecoveredNumbers.text = NumberFormat.getInstance().format(nationalDailyData.cumulative_recovered)
-                binding.RecoveredNumbersToday.text = NumberFormat.getInstance().format(nationalDailyData.recovered)
+                    binding.RecoveredNumbers.text =
+                        NumberFormat.getInstance().format(nationalDailyData.cumulative_recovered)
+                    binding.RecoveredNumbersToday.text =
+                        NumberFormat.getInstance().format(nationalDailyData.recovered)
 
-                binding.DeadNumbers.text = NumberFormat.getInstance().format(nationalDailyData.cumulative_deaths)
-                binding.DeathNumbersToday.text = NumberFormat.getInstance().format(nationalDailyData.deaths)
+                    binding.DeadNumbers.text =
+                        NumberFormat.getInstance().format(nationalDailyData.cumulative_deaths)
+                    binding.DeathNumbersToday.text =
+                        NumberFormat.getInstance().format(nationalDailyData.deaths)
 
-                binding.TestsNumbers.text = NumberFormat.getInstance().format(nationalDailyData.cumulative_testing)
-                binding.TestsNumbersToday.text = NumberFormat.getInstance().format(nationalDailyData.testing)
+                    binding.TestsNumbers.text =
+                        NumberFormat.getInstance().format(nationalDailyData.cumulative_testing)
+                    binding.TestsNumbersToday.text =
+                        NumberFormat.getInstance().format(nationalDailyData.testing)
 
-                binding.lastUpdated.text = "Last updated at: " + outputDateFormat.format(nationalDailyData.date)
+                    binding.lastUpdated.text =
+                        "Last updated at: " + outputDateFormat.format(nationalDailyData.date)
 
-                binding.piechart.addPieSlice(PieModel("Confirmed", nationalDailyData.cumulative_cases.toFloat(), getColor(R.color.yellow)))
-                binding.piechart.addPieSlice(PieModel("Active", nationalDailyData.active_cases.toFloat(), getColor(R.color.blue_pie)))
-                binding.piechart.addPieSlice(PieModel("Recovered", nationalDailyData.cumulative_recovered.toFloat(), getColor(R.color.green_pie)))
-                binding.piechart.addPieSlice(PieModel("Death", nationalDailyData.cumulative_deaths.toFloat(), getColor(R.color.red_pie)))
+                    binding.piechart.addPieSlice(
+                        PieModel(
+                            "Confirmed",
+                            nationalDailyData.cumulative_cases.toFloat(),
+                            getColor(R.color.yellow)
+                        )
+                    )
+                    binding.piechart.addPieSlice(
+                        PieModel(
+                            "Active",
+                            nationalDailyData.active_cases.toFloat(),
+                            getColor(R.color.blue_pie)
+                        )
+                    )
+                    binding.piechart.addPieSlice(
+                        PieModel(
+                            "Recovered",
+                            nationalDailyData.cumulative_recovered.toFloat(),
+                            getColor(R.color.green_pie)
+                        )
+                    )
+                    binding.piechart.addPieSlice(
+                        PieModel(
+                            "Death",
+                            nationalDailyData.cumulative_deaths.toFloat(),
+                            getColor(R.color.red_pie)
+                        )
+                    )
 
-                binding.piechart.startAnimation()
+                    binding.piechart.startAnimation()
+                }
+
             }
 
             override fun onFailure(call: Call<Summary>, t: Throwable) {
@@ -92,6 +137,7 @@ class MainActivity : AppCompatActivity() {
         })
 
         covidService.getProvincialData().enqueue(object : Callback<Summary> {
+            @RequiresApi(Build.VERSION_CODES.M)
             override fun onResponse(call: Call<Summary>, response: Response<Summary>) {
                 Log.i(TAG, "onResponse $response")
                 val nationalData = response.body()
@@ -101,7 +147,18 @@ class MainActivity : AppCompatActivity() {
                     return
                 }
 
+                nationalData.summary.toMutableList().add(nationalDailyData)
+
                 provincialData = nationalData.summary.groupBy { it.province }
+
+                val data = provincialData[province]?.first()
+
+                if (data != null) {
+                    setValue(data)
+                }
+
+
+
                 Log.i(TAG, "Update graph with new daily national numbers")
             }
 
@@ -110,7 +167,82 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
-        binding.countryName.setOnClickListener { startActivity(Intent(this, ProvinceActivity::class.java)) }
+        binding.countryName.setOnClickListener {
+            startActivity(
+                Intent(
+                    this,
+                    ProvinceActivity::class.java
+                )
+            )
+        }
+
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun setValue (provincialData: CovidData) {
+        val outputDateFormat = SimpleDateFormat("MMM dd, yyyy", Locale.US)
+
+        binding.countryName.text = province
+
+        binding.ConfirmedNumbers.text =
+            NumberFormat.getInstance().format(provincialData.cumulative_cases)
+        binding.ConfirmedNumbersToday.text =
+            NumberFormat.getInstance().format(provincialData.active_cases_change)
+
+        binding.ActiveNumbers.text =
+            NumberFormat.getInstance().format(provincialData.active_cases)
+        binding.ActiveNumbersToday.text =
+            NumberFormat.getInstance().format(provincialData.active_cases_change)
+
+        binding.RecoveredNumbers.text =
+            NumberFormat.getInstance().format(provincialData.cumulative_recovered)
+        binding.RecoveredNumbersToday.text =
+            NumberFormat.getInstance().format(provincialData.recovered)
+
+        binding.DeadNumbers.text =
+            NumberFormat.getInstance().format(provincialData.cumulative_deaths)
+        binding.DeathNumbersToday.text =
+            NumberFormat.getInstance().format(provincialData.deaths)
+
+        binding.TestsNumbers.text =
+            NumberFormat.getInstance().format(provincialData.cumulative_testing)
+        binding.TestsNumbersToday.text =
+            NumberFormat.getInstance().format(provincialData.testing)
+
+        binding.lastUpdated.text =
+            "Last updated at: " + outputDateFormat.format(provincialData.date)
+
+        binding.piechart.addPieSlice(
+            PieModel(
+                "Confirmed",
+                provincialData.cumulative_cases.toFloat(),
+                getColor(R.color.yellow)
+            )
+        )
+        binding.piechart.addPieSlice(
+            PieModel(
+                "Active",
+                provincialData.active_cases.toFloat(),
+                getColor(R.color.blue_pie)
+            )
+        )
+        binding.piechart.addPieSlice(
+            PieModel(
+                "Recovered",
+                provincialData.cumulative_recovered.toFloat(),
+                getColor(R.color.green_pie)
+            )
+        )
+        binding.piechart.addPieSlice(
+            PieModel(
+                "Death",
+                provincialData.cumulative_deaths.toFloat(),
+                getColor(R.color.red_pie)
+            )
+        )
+
+        binding.piechart.startAnimation()
 
     }
 }
--- app/src/main/java/com/example/worldcovid19tracker/ProvinceAdapter.kt
+++ app/src/main/java/com/example/worldcovid19tracker/ProvinceAdapter.kt
@@ -1,22 +1,31 @@
 package com.example.worldcovid19tracker
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.worldcovid19tracker.databinding.ProvinceItemLayoutBinding
 import java.text.NumberFormat
 
-class ProvinceAdapter (private val list: MutableList<CovidData>) :
+class ProvinceAdapter(private val list: MutableList<CovidData>) :
     RecyclerView.Adapter<ProvinceAdapter.ProvinceViewHolder>() {
-    inner class ProvinceViewHolder(val binding: ProvinceItemLayoutBinding) : RecyclerView.ViewHolder(binding.root)
+    inner class ProvinceViewHolder(val binding: ProvinceItemLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProvinceViewHolder {
-        return ProvinceViewHolder(ProvinceItemLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+        return ProvinceViewHolder(
+            ProvinceItemLayoutBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
     }
 
     override fun onBindViewHolder(holder: ProvinceViewHolder, position: Int) {
         holder.binding.provinceName.text = list[position].province
-        holder.binding.provinceCases.text = NumberFormat.getInstance().format(list[position].cumulative_cases)
+        holder.binding.provinceCases.text =
+            NumberFormat.getInstance().format(list[position].cumulative_cases)
         holder.binding.provincialFlag.setImageResource(
             when (list[position].province) {
                 "Alberta" -> R.drawable.ic_flag_of_alberta
@@ -36,6 +45,12 @@ class ProvinceAdapter (private val list: MutableList<CovidData>) :
                 else -> R.drawable.ic_flag_of_canada
             }
         )
+
+        holder.binding.root.setOnClickListener {
+            val intent = Intent(it.context, MainActivity::class.java)
+            intent.putExtra("province", list[position].province)
+            it.context.startActivity(intent)
+        }
     }
 
     override fun getItemCount(): Int = list.size
